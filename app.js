423a99ed587797d61a4c7cbdbe75e6e0b59f70bd
Rishant Agarwal <bindasrishant@gmail.com>
2016-02-18 06:42:21 +0000

    Deleting unrequired stuff

--- app.js
+++ app.js
@@ -4,18 +4,11 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/bower_components'));  
+
 app.get('/', function(req, res,next) {  
-    res.sendFile(__dirname + '/index.html');
-});
-
-
-app.get('/remote', function(req, res,next) {  
     res.sendFile(__dirname + '/remote.html');
 });
 
-app.get('/draw', function(req, res,next) {  
-    res.sendFile(__dirname + '/draw.html');
-});
 app.all('*', function(req, res, next) {
        res.header("Access-Control-Allow-Origin", "*");
        res.header("Access-Control-Allow-Headers", "X-Requested-With");
